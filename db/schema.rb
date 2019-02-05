7a4caf10443be1946946a14f30375766eda16bd2
Simon Cooper <cooper.sf@gmail.com>
2019-02-05 16:33:30 +0000

    adding tours field to distillery

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_01_114225) do
+ActiveRecord::Schema.define(version: 2019_02_05_163307) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 2019_02_01_114225) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "slug"
+    t.boolean "tours"
   end
 
   create_table "gins", force: :cascade do |t|
